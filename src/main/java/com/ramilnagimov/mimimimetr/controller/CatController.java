fef620ede14c71b472be8eeadcd9efc8a74f3b4b
Ramil Nagimov <nagram77@gmail.com>
2021-07-31 19:10:00 +0300

    Fixed model attribute error.

--- src/main/java/com/ramilnagimov/mimimimetr/controller/CatController.java
+++ src/main/java/com/ramilnagimov/mimimimetr/controller/CatController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -24,38 +23,45 @@ public class CatController {
     }
 
     @GetMapping("/voting")
-    public String voting(Model model) {
+    public String showNextPairOfCats(Model model, @RequestBody(required = false) Long id) {
 
         List<Cat> cats = catService.fetchNextPairOfCats();
-        if (cats.isEmpty())
-        {
-            List<NameAndImageForViewTopCats> catAndImageForViewTopNames = new LinkedList<>();
-            List<Cat> topCats = catService.getTopCats();
-            for(Cat cat: topCats) {
-                String catBase64 = ImageEncoder.encodeImageToBase64(cat.getImage());
-                NameAndImageForViewTopCats newCat = new NameAndImageForViewTopCats(cat.getCatsName(), catBase64);
-                catAndImageForViewTopNames.add(newCat);
-            }
-            model.addAttribute("catList", catAndImageForViewTopNames);
-            return "top-cats";
+        if (cats.isEmpty()) {
+            showTopCats(model);
+            return "/top-cats";
         }
 
         Cat catLeft = cats.get(0);
         String catLeftBase64 = ImageEncoder.encodeImageToBase64(catLeft.getImage());
         model.addAttribute("catLeft", catLeft);
         model.addAttribute("catLeftBase64", catLeftBase64);
+        model.addAttribute("catLeftID", catLeft.getId());
+        model.addAttribute("catLeftName", catLeft.getCatsName());
 
         Cat catRight = cats.get(1);
         String catRightBase64 = ImageEncoder.encodeImageToBase64(catRight.getImage());
         model.addAttribute("catRight", catRight);
         model.addAttribute("catRightBase64", catRightBase64);
+        model.addAttribute("catRightID", catRight.getId());
+        model.addAttribute("catRightName", catRight.getCatsName());
 
         return "/voting";
     }
 
-    @PostMapping ("/voting")
-    public void voting(@RequestBody(required=false) Long id ) {
-        if(id !=null) catService.updateCatScore(id);
+    public void showTopCats(Model model) {
+        List<NameAndImageForViewTopCats> catAndImageForViewTopNames = new LinkedList<>();
+        List<Cat> topCats = catService.getTopCats();
+        for (Cat cat : topCats) {
+            String catBase64 = ImageEncoder.encodeImageToBase64(cat.getImage());
+            NameAndImageForViewTopCats newCat = new NameAndImageForViewTopCats(cat.getCatsName(), catBase64);
+            catAndImageForViewTopNames.add(newCat);
+        }
+        model.addAttribute("catList", catAndImageForViewTopNames);
+    }
+
+    @PostMapping("/voting")
+    public void voting(@RequestBody(required = false) Long id) {
+            catService.updateCatScore(id);
     }
 }
 
